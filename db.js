8212f9cdfc6ccee40ee07854aa1de99cbe4ae8e9
fh2303 <fepaul82@gmail.com>
2026-03-24 21:20:13 -0400

    Updated schemas

--- db.js
+++ db.js
@@ -2,24 +2,25 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, unique: true },
-  hash: { type: String },
+  name: String,
+  age: Number,
+  email: { type: String, required: true, lowercase: true },
+  hash: { type: String, required: true },
   polls: [mongoose.Schema.Types.ObjectId],
 });
 
-const PollSchema = new mongoose.Schema(
-  {
-    creator: mongoose.Schema.Types.ObjectId,
-    eventDetails: {
-      title: { type: String, required: true },
-      locationName: String,
-      time: Date,
-    },
-
-    votes: { type: Number, default: 0 },
-    voters: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+const PollSchema = new mongoose.Schema({
+  creator: mongoose.Schema.Types.ObjectId,
+  eventDetails: {
+    title: { type: String, required: true },
+    locationName: String,
+    time: Date,
   },
-  { createdAt: true }
-);
+
+  votes: { type: Number, default: 0 },
+  voters: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, immutable: true, default: () => Date.now() },
+});
 
 export const User = mongoose.model("User", UserSchema);
 export const Poll = mongoose.model("Poll", PollSchema);
