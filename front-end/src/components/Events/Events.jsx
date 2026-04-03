72a5de0e61d5047c892123e50f3611667d7ca1d3
fh2303 <fepaul82@gmail.com>
2026-04-03 19:49:10 -0400

    Implemented posting messages from db cluster

--- front-end/src/components/Events/Events.jsx
+++ front-end/src/components/Events/Events.jsx
@@ -1,10 +1,19 @@
 import styles from "./Events.module.css";
+import { socket } from "../../socket.js";
 
 export function Events({ events }) {
   return (
     <ul className={styles.messages}>
       {events.map((event, index) => (
-        <li key={index}>{event}</li>
+        <li key={event._id || index}>
+          <span>{event.content || "Empty"}</span>
+          <button
+            onClick={() => socket.emit("delete message", event._id)}
+            className={styles.button}
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
