702b1a27fc906ccdb658dc4d50b62a09eab0628f
fh2303 <fepaul82@gmail.com>
2026-04-04 22:14:55 -0400

    CHanged url

--- front-end/src/socket.js
+++ front-end/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.BACKEND_URL || "http://localhost:4000";
+const URL = "https://final-project-fh2303.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false,
