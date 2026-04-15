6fd504cf968abb853377c778b3b73a2895565064
fh2303 <fepaul82@gmail.com>
2026-04-14 23:31:29 -0400

    adjusted route

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../front-end/dist", "index.html"));
 });
 
