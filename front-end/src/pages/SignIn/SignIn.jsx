4ae1b835638845de955cc1843c98a66fd96f3f81
fh2303 <fepaul82@gmail.com>
2026-04-14 22:04:22 -0400

    Finished login page styling

--- front-end/src/pages/SignIn/SignIn.jsx
+++ front-end/src/pages/SignIn/SignIn.jsx
@@ -1,29 +1,43 @@
 import styles from "../loginPages.module.css";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function SignIn() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {};
   return (
     <div className={styles.body}>
       <section className="sign-in">
         <div className={styles.main}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <p className={styles.signTitle}>Sign in</p>
+            <Link to="/register" className={styles.switchPage}>
+              or Register?
+            </Link>
             <div className={styles.usernameDiv}>
-              <input name="email" type="email" placeholder="Email" required />
+              <input
+                name="email"
+                className={styles.email}
+                type="email"
+                placeholder="Email"
+                required
+              />
             </div>
             <div className={styles.passwordDiv}>
               <input
                 name="password"
+                className={styles.password}
                 type="password"
                 placeholder="Password"
                 required
               />
-            </div>
-            <div className={styles.buttonWrapper}>
-              <button type="submit" className={styles.signButton}>
-                Jump in!
-              </button>
-            </div>
+            </div>{" "}
+            {error && <p className={styles.errorMsg}>{error}</p>}
+            <button type="submit" className={styles.signButton}>
+              Jump in!
+            </button>
           </form>
         </div>
       </section>
