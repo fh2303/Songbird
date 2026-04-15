a48262242f43f7835daab9f8ce72bdd2e1bbe999
fh2303 <fepaul82@gmail.com>
2026-04-14 23:41:07 -0400

    Adjusted get route

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../front-end/dist", "index.html"));
 });
 
