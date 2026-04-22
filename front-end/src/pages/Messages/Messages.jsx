e870cc5cce5b221eb9b046c5f1db80271d65d6d1
fh2303 <fepaul82@gmail.com>
2026-04-21 22:56:37 -0400

    Saving work, adding add room button

--- front-end/src/pages/Messages/Messages.jsx
+++ front-end/src/pages/Messages/Messages.jsx
@@ -1,16 +1,25 @@
 import styles from "./Messages.module.css";
 import { socket } from "../../socket.js";
+import { useState } from "react";
 
 function Messages() {
+  const [visible, setVisible] = useState(false);
+
   const roomName = "hi";
   function join(roomName) {
     socket.emit("join room", roomName);
     // setActiveRoom(roomName)
   }
 
+  function createRoom() {}
+
   return (
     <div className={styles.main}>
-      <div className={styles.header}></div>
+      <div className={styles.header}>
+        <button className={styles.add} onClick={() => createRoom()}>
+          Add
+        </button>
+      </div>
       <button className={styles.room} onClick={() => join(roomName)}>
         Room
       </button>
